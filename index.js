6bd6fd94c76ed10046664a737feb56f77871c17b
pranjalM0408 <161703319+pranjalM0408@users.noreply.github.com>
2024-02-29 15:05:49 +0530

    Add initial practice project

---
+++ index.js
@@ -0,0 +1,33 @@
+alert("working!!");
+//player 1
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png to dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; // images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomImageSource);
+
+//player 2
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🏴Player 1 Wins!";
+
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🏴"
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
